9eb259b642f2fb1fb99a9320e2aedbe341ca028d
howdyev <phain118@outlook.com>
2023-02-16 15:22:47 -0500

    Option2 - Practice2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,3 +13,21 @@ export const onClientEntry = () => {
       console.log(`# IntersectionObserver is polyfilled!`)
     }
 }
+
+const transitionDelay = 500
+
+export const shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition,
+}) => {
+    if (location.action === 'PUSH') {
+        window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
+    } else {
+        const savedPosition = getSavedScrollPosition(location)
+        window.setTimeout(
+            () => window.scrollTo(...(savedPosition || [0, 0])),
+            transitionDelay
+        )
+    }
+    return false
+    }
